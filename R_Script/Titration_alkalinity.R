fc2e2006a686bf86126231b99f7b7fc9ba5b5249
Jérémy T. Carlot <47741658+JayCrlt@users.noreply.github.com>
2023-06-01 18:07:41 +0200

    PI Curves addition

--- R_Script/Titration_alkalinity.R
+++ R_Script/Titration_alkalinity.R
@@ -103,8 +103,7 @@ sample_name <- append(sample_name, p[1, 8])
 }
 
 Results <- cbind(At * 1000000, list_files) %>% as_tibble() %>% rename(At = V1) %>% dplyr::select(list_files, At)
-write.table(Results, "Data/Spring_2023/Transplants/Alkalinity/Results_TA/Results_TA.csv", sep = ";", col.names = TRUE,
-            row.names = FALSE)
+# write.table(Results, "Data/Spring_2023/Transplants/Alkalinity/Results_TA/Results_TA.csv", sep = ";", col.names = T, row.names = F)
 print(paste("The alkalinity of your sample is: ", (At * 1000000), sep = ""))
 
 # usefull fumctions
