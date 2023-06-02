bc0788a9fd2b3593c94d9f4ef0d082b8ab760781
Jérémy T. Carlot <47741658+JayCrlt@users.noreply.github.com>
2023-06-02 10:11:11 +0200

    reorganisation

--- R_Script/Alkalinity – T0.R
+++ R_Script/Alkalinity – T0.R
@@ -22,7 +22,7 @@ if (Tile_concerned$Label[i] == "T0_t1_ELOW_tile_01") {
         Tile_concerned$Start_incubation[i] = Tile_concerned$Start_incubation[i] } }
 
 ### Alkalinity
-Alkalinity_dataset <- read_excel("Data/Spring_2023/Transplants/Alkalinity/BenthFun_Summary.xlsx") %>% 
+Alkalinity_dataset <- read_excel("Data/Spring_2023/BenthFun_Alkalinity.xlsx") %>% 
   drop_na(`Batch mean`) %>% dplyr::filter(`Sample id` != "CRM1") %>% 
   dplyr::select(-c(`ALK batch`, `delta batch`, note))
 
